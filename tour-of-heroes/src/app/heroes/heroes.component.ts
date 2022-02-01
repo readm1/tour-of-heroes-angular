f81e0f45954cd431c5932236a69f175bbc9dac10
mread1 <M.Read1@elsevier.com>
2022-02-01 15:36:08 +0000

    observable data

--- tour-of-heroes/src/app/heroes/heroes.component.ts
+++ tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -14,7 +14,7 @@ export class HeroesComponent implements OnInit {
   }
 
   getHeroes(): void {
-    this.heroes = this.heroService.getHeroes();
+    this.heroService.getHeroes().subscribe((heroes) => (this.heroes = heroes));
   }
 
   heroes: Hero[] = [];
